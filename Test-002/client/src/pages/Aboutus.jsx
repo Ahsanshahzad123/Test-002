b0f51d7210b0c9d4f41885ebf4835e8efdc7981d
Ahsan Shahzad <47296638+Ahsanshahzad123@users.noreply.github.com>
2025-12-24 15:58:31 +0500

    Update Best Sellers from Top Brands, add About Us page to navbar, and fix backend API routing for frontend communication

---
+++ Test-002/client/src/pages/Aboutus.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "../components/Carousel";
+import ShopBy from "../components/ShopBy";
+import GenInfo, { Brands } from "../components/GenInfo";
+
+const AboutUs = () => {
+  return (
+    <div className="max-w-screen-xl xs:w-[95vw] xs:max-w-[95vw] md:w-full mx-auto ">
+      About us
+    </div>
+  );
+};
+
+export default AboutUs;
